e3a6068201f1de093d0503f3393d2826b6038065
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-08-30 19:10:25 +0200

    add assetPrefix for prod

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   reactStrictMode: false,
+  assetPrefix: isProd
+    ? 'https://projects.msonnberger.com/metro-vienna-monitor'
+    : '',
 };
