443cfe818e118fb2edd441b46f5d182172b73c3e
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-08-31 08:05:01 +0200

    add basePath to config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  reactStrictMode: false,
-  assetPrefix: isProd
-    ? 'https://projects.msonnberger.com/metro-vienna-monitor'
-    : '',
+  reactStrictMode: true,
+  basePath: isProd ? '/metro-vienna-monitor' : '',
 };
